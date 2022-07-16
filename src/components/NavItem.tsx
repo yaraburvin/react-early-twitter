e30ae381c853493a0fd2532b5edc3120a342ff4f
yarab <yaraburvin@gmail.com>
2022-07-16 12:35:06 +0300

    Change in navigation menu style

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,9 +4,9 @@ interface NavItemProps {
 
 function NavItem(props: NavItemProps): JSX.Element {
   return (
-    <>
-      <a href="#">{props.label}</a>
-      <span> | </span>
+    <>      
+      <span> {"->"} </span>
+      <a href="#">{"~" + props.label.toUpperCase()}</a>
     </>
   );
 }
